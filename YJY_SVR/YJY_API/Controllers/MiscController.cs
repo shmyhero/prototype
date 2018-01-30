f4b4536a22db3b2fa2bb8537480851f435aa06e1
Ivan Jiang <jiangyi_1985@hotmail.com>
2018-01-30 14:41:33 +0800

    add SVR project files

---
+++ YJY_SVR/YJY_API/Controllers/MiscController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace YJY_SVR.Controllers
+{
+    [RoutePrefix("api/misc")]
+    public class MiscController : ApiController
+    {
+        [HttpGet]
+        [ActionName("version")]
+        public HttpResponseMessage GetVersion()
+        {
+            //string dbName = db.Database.Connection.Database;
+
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+#if DEBUG
+                "YJY API STATUS: OK [build=DEBUG]" +
+#else
+                "YJY API STATUS: OK [build=RELEASE]" +
+#endif
+                    " -- v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()
+                //+ " -- DB=[" + dbName + "]"
+                );
+        }
+    }
+}
