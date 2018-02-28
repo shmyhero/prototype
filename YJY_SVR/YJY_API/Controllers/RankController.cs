2ecc47ad802576bc3ca6185603f22c8b7e41c3c4
Ivan Jiang <jiangyi_1985@hotmail.com>
2018-02-28 14:40:42 +0800

    user rank 2w API

---
+++ YJY_SVR/YJY_API/Controllers/RankController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YJY_COMMON.Model.Context;
+using YJY_COMMON.Util;
+using YJY_SVR.Controllers.Attributes;
+using YJY_SVR.DTO;
+
+namespace YJY_SVR.Controllers
+{
+    [RoutePrefix("api/rank")]
+    public class RankController : YJYController
+    {
+        public RankController(YJYEntities db) : base(db)
+        {
+        }
+
+        [HttpGet]
+        [Route("user/plClosed/2w")]
+        public List<UserDTO> GetUserRankPL2w()
+        {
+            var twoWeeksAgo = DateTimes.GetChinaToday().AddDays(-13);
+            var twoWeeksAgoUtc = twoWeeksAgo.AddHours(-8);
+
+            var userDTOs = db.Positions.Where(o => o.ClosedAt != null && o.ClosedAt >= twoWeeksAgoUtc)
+                .GroupBy(o => o.UserId)
+                .Join(db.Users,g=>g.Key,u=>u.Id,(g,u)=> new UserDTO()
+                {
+                    id = g.Key.Value,
+
+                    nickname = u.Nickname,
+                    picUrl = u.PicUrl,
+
+                    posCount = g.Count(),
+                    winRate = (decimal)g.Count(p => p.PL > 0) / g.Count(),
+                    roi = g.Sum(p => p.PL.Value) /g.Sum(p => p.Invest.Value),
+                }).OrderByDescending(o => o.roi).ToList();
+
+            return userDTOs;
+        }
+    }
+}
