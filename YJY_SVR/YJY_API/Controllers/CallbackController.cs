257a194d9d3b3f3357361b92f597a578df56e933
Ivan Jiang <jiangyi_1985@hotmail.com>
2018-04-16 15:22:22 +0800

    THT deposit callback API

---
+++ YJY_SVR/YJY_API/Controllers/CallbackController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YJY_COMMON.Model.Context;
+using YJY_SVR.DTO.FormDTO;
+
+namespace YJY_SVR.Controllers
+{
+    [RoutePrefix("api/callback")]
+    public class CallbackController : YJYController
+    {
+        public CallbackController(YJYEntities db) : base(db)
+        {
+        }
+
+        private const string CALLBACK_AUTH_TOKEN = "jR7cB9s6n2I0C4zP1xZ6b92Mki0Q3Ae7G1L3vU5hoT8xD5Fy3Ux9bR1wO5Hb7ec4HJ6Es2oC";
+
+        [HttpPost]
+        [Route("THT/deposit")]
+        public bool THTDeposit(THTDepositFormDTO form)
+        {
+            var authorization = Request.Headers.Authorization;
+            
+            if (authorization?.Parameter == null || authorization.Parameter != CALLBACK_AUTH_TOKEN)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "invalid auth token"));
+
+            return true;
+        }
+    }
+}
---
+++ YJY_SVR/YJY_API/DTO/FormDTO/CallbackFormDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YJY_SVR.DTO.FormDTO
+{
+    public class THTDepositFormDTO
+    {
+        public int index { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public int value { get; set; }
+    }
+}
